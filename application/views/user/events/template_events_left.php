561e9e010b85c2dfb0dfbf4f828de0f86e48d6b1
John Mark Abril <johnmarkabril@gmail.com>
2016-11-27 21:41:27 +0800

    Updating Events User Design

--- application/views/user/events/template_events_left.php
+++ application/views/user/events/template_events_left.php
@@ -3,16 +3,19 @@
             if ( !empty($get_all_events) ) {
                 foreach ( $get_all_events as $gae ) :
         ?>
-        <div class="jumbotron product-box animated fadeInRight" style="background-color: #ffffff; padding: 20px;">
-            <div class="row">
-                <div class="col-md-12">
-                <div class="text-left title-events"><?php echo $gae->TITLE; ?></div>
-                  <div>
-                    <?php echo $gae->DESCRIPTION; ?>
-                  </div>
-                </div>
-            </div>
-        </div>
+                    <div class="" style="background-color: #ffffff; padding: 20px;">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="text-left title-events">
+                                    <?php echo $gae->TITLE; ?>
+                                    <span class="pull-right" style="font-size: 10px;"><?php echo $gae->DATE; ?></span>
+                                </div>
+                                <div>
+                                    <?php echo $gae->DESCRIPTION; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
         <?php 
                 endforeach;
             }
